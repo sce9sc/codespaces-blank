796fb2442119e1edbf518c6f08bf93f650b70817
Stavros Charitakis <sce9sc@hotmail.com>
2023-05-31 20:19:58 +0300

    test5

--- relay.js
+++ relay.js
@@ -26,9 +26,9 @@ openssl x509 -req -sha256 -days 365 -in snakeoil.csr -signkey snakeoil.key -out 
 
   const node = await createLibp2p({
     addresses: {
-      listen: ["/ip4/0.0.0.0/tcp/443/ws"],
+      listen: ["/ip4/0.0.0.0/tcp/3000/ws"],
       // TODO check "What is next?" section
-      announce: ["/dns4/libp2p-relay-l2d1.onrender.com/tcp/443/wss", "/ip4/216.24.57.253/tcp/443/wss"],
+      announce: ["/dns4/libp2p-relay-l2d1.onrender.com/tcp/3000/wss", "/ip4/216.24.57.253/tcp/3000/wss"],
     },
     transports: [
       webSockets({
