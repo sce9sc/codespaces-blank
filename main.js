4028a61ee96cee9a950ae6103060ea5605a04954
Stavros Charitakis <sce9sc@hotmail.com>
2023-05-31 11:52:06 +0300

    adding autonat + announce

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ async function main() {
     const { yamux } = await import("@chainsafe/libp2p-yamux");
     const { circuitRelayServer } = await import("libp2p/circuit-relay");
     const { identifyService } = await import("libp2p/identify");
+    const { autoNATService } = await import("libp2p/autonat");
+    const { uPnPNATService } = await import("libp2p/upnp-nat");
   
     const node = await createLibp2p({
       addresses: {
         listen: ["/ip4/0.0.0.0/tcp/4410/ws","/ip4/0.0.0.0/tcp/4430/wss"],//"/ip4/3.75.158.163/tcp/8085/ws","/ip4/3.125.183.140/tcp/8085/ws","/ip4/35.157.117.28/tcp/8085/ws"],
         // TODO check "What is next?" section
-        announce: ['/dns4/libp2p-relay-l2d1.onrender.com/tcp/4430/wss']
+        announce: ['/dns4/libp2p-relay-l2d1.onrender.com/tcp/4430/wss',"/dns4/libp2p-relay-l2d1.onrender.com/tcp/4410/ws'"]
       },
       transports: [webSockets()],
       connectionEncryption: [noise()],
@@ -19,6 +21,12 @@ async function main() {
       services: {
         identify: identifyService(),
         relay: circuitRelayServer(),
+        uPnPNAT: uPnPNATService({
+          /** UPnP NAT options **/
+        }),
+        autoNAT: autoNATService({
+          /** AutoNAT options **/
+        }),
       },
     });
   
