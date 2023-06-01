a05c035aa31fda1b6b6d3d0efca64ac36d3a2505
Stavros Charitakis <sce9sc@hotmail.com>
2023-06-01 21:22:15 +0300

    change port to 9000

--- relay.js
+++ relay.js
@@ -29,10 +29,10 @@ openssl x509 -req -sha256 -days 365 -in snakeoil.csr -signkey snakeoil.key -out 
 
   const node = await createLibp2p({
     addresses: {
-      listen: ["/ip4/0.0.0.0/tcp/80/ws"],
+      listen: ["/ip4/0.0.0.0/tcp/9000/ws"],
       // TODO check "What is next?" section
       announce: [
-        "/dns4/libp2p-relay-l2d1.onrender.com/tcp/443/wss",
+        "/dns4/libp2p-relay-l2d1.onrender.com/tcp/9000/ws",
         // "/ip4/216.24.57.253/tcp/443/wss",
         // "/dns4/libp2p-relay-l2d1.onrender.com/tcp/80/ws"
     ],
